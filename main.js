f68ed291a086cd4759b3f3c3e08e31d1a7bd4c8f
Nesthor Heads <nestorcabezas@gmail.com>
2022-06-22 06:15:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(".imagen").click(function(e){
+	var enlaceImagen = e.target.src;
+	var data =e.target.src;/* $(this).attr("data");*/
+	var lightbox = '<div class="ligthbox">'+
+						'<img src="'+enlaceImagen+'" alt="" id="zoom_mw" data-zoom-image="'+enlaceImagen+'">'+
+						'<div class="btn-close">X</div>'+
+					'</div>';
+
+	$("body").append(lightbox)
+	$("#zoom_mw").elevateZoom({
+		scrollZoom : true,
+		cursor: "crosshair",
+		zoomWindowOffetx: 15
+	});
+	$(".btn-close").click(function(){
+		 $(".ligthbox").remove();
+		//$(".ligthbox").hide('slow');
+	})
+
+})
